f82531b11bb64068116a0b76f857a73dbae3c620
lseseri <lhseseri@gmail.com>
2025-01-22 19:47:32 -0800

    adding automatic nav menu

--- global.js
+++ global.js
@@ -4,14 +4,46 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume'},
+  { url: 'https://github.com/lseseri', title: 'Github'},
+];
 
-const navLinks = $$("nav a");
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-if (currentLink) {
-  // or if (currentLink !== undefined)
-  currentLink.classList.add('current');
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = '../' + url;
+  }
+  // another way to conditionally create links 
+  // url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  } 
+  // another way to do this 
+  // a.classList.toggle(
+  //   'current',
+  //   a.host === location.host && a.pathname === location.pathname
+  // );
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
 }
+
