f278f78465eab43bd2492af6e9ed73718bb7a020
lseseri <lhseseri@gmail.com>
2025-01-22 19:07:45 -0800

    adding js file and automatic current link page

---
+++ global.js
@@ -0,0 +1,17 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink.classList.add('current');
+}
